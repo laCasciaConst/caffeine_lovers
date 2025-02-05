8e69605413099e1e19906f5e7e3473b06b0ce6e3
laCasciaConst <lshin@esad-orleans.fr>
2025-02-05 16:53:30 +0100

    roomManager

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    [SerializeField] GameObject roomPrefab;
+    [SerializeField] private int maxRooms = 10;
+    [SerializeField] private int minRooms = 3;
+
+    int roomWidth = 20;
+    int roomHeight = 12;
+    int gridSizeX = 10;
+    int gridSizeY = 10;
+
+    private List<GameObject> roomObjects = new List<GameObject>();
+    private Queue<Vector2Int> roomQueue = new Queue<Vector2Int>();
+    private int[,] roomGrid;
+    private int roomCount;
+    private bool generationComplete = false;
+    private void Start()
+    {
+        roomGrid = new int[gridSizeX, gridSizeY];
+        roomQueue = new Queue<Vector2Int>();
+
+        Vector2Int initialRoomIndex = new Vector2Int(gridSizeX / 2, gridSizeY / 2);
+        StartRoomGenerationFromRoom(initialRoomIndex);
+    }
+
+    private void Update()
+    {
+        if (roomQueue.Count > 0 && roomCount < maxRooms && !generationComplete)
+        {
+            Vector2Int roomIndex = roomQueue.Dequeue();
+            int gridX = roomIndex.x;
+            int gridY = roomIndex.y;
+
+            TryGenerateRoom(new Vector2Int(gridX - 1, gridY));
+            TryGenerateRoom(new Vector2Int(gridX + 1, gridY));
+            TryGenerateRoom(new Vector2Int(gridX, gridY + 1));
+            TryGenerateRoom(new Vector2Int(gridX, gridY - 1));
+        }
+        else if (!generationComplete)
+        {
+            Debug.Log($"Generation Complete, {roomCount} rooms created");
+            generationComplete = true;
+        }
+    }
+    private void StartRoomGenerationFromRoom(Vector2Int roomIndex)
+    {
+        roomQueue.Enqueue(roomIndex);
+        int x = roomIndex.x;
+        int y = roomIndex.y;
+        roomGrid[x, y] = 1;
+        roomCount++;
+        var initialRoom = Instantiate(roomPrefab, GetPositionFromGridIndex(roomIndex), Quaternion.identity);
+        initialRoom.name = $"Room-{roomCount}";
+        initialRoom.GetComponent<Room>().RoomIndex = roomIndex;
+        roomObjects.Add(initialRoom);
+    }
+
+    private bool TryGenerateRoom(Vector2Int roomIndex)
+    {
+        int x = roomIndex.x;
+        int y = roomIndex.y;
+
+        roomQueue.Enqueue(roomIndex);
+        roomGrid[x, y] = 1;
+        roomCount++;
+
+        var newRoom = Instantiate(roomPrefab, GetPositionFromGridIndex(roomIndex), Quaternion.identity);
+        newRoom.name = $"Room-{roomCount}";
+        newRoom.GetComponent<Room>().RoomIndex = roomIndex;
+        roomObjects.Add(newRoom);
+
+        return true;
+    }
+
+    private Vector3 GetPositionFromGridIndex(Vector2Int gridIndex)
+    {
+        int gridX = gridIndex.x;
+        int gridY = gridIndex.y;
+        return new Vector3(roomWidth * (gridX - gridSizeX / 2),
+        roomHeight * (gridY - gridSizeY / 2));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Color gizmoColor = new Color(0, 1, 1, 0.05f);
+        Gizmos.color = gizmoColor;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 position = GetPositionFromGridIndex(new Vector2Int(x, y));
+                Gizmos.DrawWireCube(position, new Vector3(roomWidth, roomHeight, 1));
+            }
+        }
+    }
+}
